52a105a4e97530a389fb382912b212a50c8a3fbc
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-02-10 12:45:34 +1100

    Initial commit

---
+++ NeoForge/src/main/java/com/author/examplemod/ExampleModForge.java
@@ -0,0 +1,20 @@
+package com.author.examplemod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+
+@Mod(ModConstants.MOD_ID)
+public class ExampleModForge {
+
+    public ExampleModForge(IEventBus modEventBus) {
+        ModConstants.LOGGER.info("Hello Forge!");
+        ExampleModCommon.initialize();
+        NeoForge.EVENT_BUS.addListener(this::onItemTooltip);
+    }
+
+    private void onItemTooltip(ItemTooltipEvent event) {
+        ExampleModCommon.onItemTooltip(event.getItemStack(), event.getContext(), event.getFlags(), event.getToolTip());
+    }
+}
