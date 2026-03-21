98c18f915bb610948dd674bf875721db9681f56a
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-03-21 22:48:59 +1100

    Update Unified.java

--- common/src/main/java/net/rebel459/unified/Unified.java
+++ common/src/main/java/net/rebel459/unified/Unified.java
@@ -1,10 +1,5 @@
 package net.rebel459.unified;
 
-import net.minecraft.tags.BiomeTags;
-import net.minecraft.world.level.biome.Biomes;
-import net.rebel459.unified.platform.HelpersImpl;
-import net.rebel459.unified.platform.UnifiedEvents;
-import net.rebel459.unified.platform.UnifiedHelpers;
 import net.rebel459.unified.registry.UnifiedDataComponents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,10 +8,6 @@ public class Unified {
 
     public static void initRegistries() {
         UnifiedDataComponents.init();
-        UnifiedHelpers.BIOME_MODIFICATIONS.register(BiomeTags.HAS_DESERT_PYRAMID, context -> {
-            context.getClimate().setPrecipitation(true);
-            context.getEffects().setWaterColor(150);
-        });
     }
 
     public static void init() {}
