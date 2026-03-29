00dd7476fc22d6013eb12ff6a74aeace250761af
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-03-29 11:05:07 +1100

    imports

--- common/src/main/java/net/rebel459/unified/platform/client/ClientHelpersImpl.java
+++ common/src/main/java/net/rebel459/unified/platform/client/ClientHelpersImpl.java
@@ -6,17 +6,14 @@ import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.particle.ParticleResources;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.material.Fluid;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
