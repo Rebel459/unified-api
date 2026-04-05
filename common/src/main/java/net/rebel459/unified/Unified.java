6f818b50101c821f354edd50fd21c5ff90c0d337
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-04-05 12:19:43 +1000

    Update Unified.java

--- common/src/main/java/net/rebel459/unified/Unified.java
+++ common/src/main/java/net/rebel459/unified/Unified.java
@@ -1,10 +1,5 @@
 package net.rebel459.unified;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.storage.loot.BuiltInLootTables;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.rebel459.unified.platform.UnifiedEvents;
 import net.rebel459.unified.registry.UnifiedDataComponents;
 import net.rebel459.unified.util.helper.StructureMusicImpl;
 
@@ -16,11 +11,6 @@ public class Unified {
 
     public static void init() {
         StructureMusicImpl.init();
-        UnifiedEvents.LootTables.modify(((table, key, provider) -> {
-            if (BuiltInLootTables.SHIPWRECK_MAP == key) {
-                table.editPool(item -> item.get() == Items.MAP, LootItem.lootTableItem(Items.NETHER_STAR), true);
-            }
-        }));
     }
 
     public static final String MOD_ID = "unified";
