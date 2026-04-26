2d73e03e7155b3592e392386c71d6a453f2983ba
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-04-26 19:03:34 +1000

    fix hopper composting crash

--- fabric/src/main/java/net/rebel459/unified/mixin/block/ComposterBlockInputContainerMixin.java
+++ fabric/src/main/java/net/rebel459/unified/mixin/block/ComposterBlockInputContainerMixin.java
@@ -3,10 +3,13 @@ package net.rebel459.unified.mixin.block;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import it.unimi.dsi.fastutil.objects.Object2FloatMap;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.rebel459.unified.registry.UnifiedDataComponents;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ComposterBlock.InputContainer.class)
@@ -14,7 +17,17 @@ public class ComposterBlockInputContainerMixin {
 
     @WrapOperation(at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z"), method = "canPlaceItemThroughFace")
     private boolean compostComponentUseItemOn(Object2FloatMap instance, Object object, Operation<Boolean> original) {
-        ItemStack stack = (ItemStack) object;
+        ItemStack stack = getStack(object);
         return original.call(instance, stack) || (stack.has(UnifiedDataComponents.COMPOST.get()) && stack.get(UnifiedDataComponents.COMPOST.get()) > 0F);
+    }
+
+    @Unique
+    private static ItemStack getStack(Object object) {
+        return switch (object) {
+            case ItemStack itemStack -> itemStack;
+            case Item item -> item.getDefaultInstance();
+            case Block block -> block.asItem().getDefaultInstance();
+            case null, default -> ItemStack.EMPTY;
+        };
     }
 }
