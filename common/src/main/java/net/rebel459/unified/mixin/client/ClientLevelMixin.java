94a0301279ff9d1125fe609695a1f5c702d81db9
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-05-01 23:05:18 +1000

    Update ClientLevelMixin.java

--- common/src/main/java/net/rebel459/unified/mixin/client/ClientLevelMixin.java
+++ common/src/main/java/net/rebel459/unified/mixin/client/ClientLevelMixin.java
@@ -1,38 +1,18 @@
 package net.rebel459.unified.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.resources.sounds.SoundInstance;
-import net.minecraft.client.sounds.MusicManager;
-import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.core.Holder;
-import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.sounds.Music;
-import net.minecraft.sounds.Musics;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
-import net.rebel459.unified.network.StructurePackets;
 import net.rebel459.unified.platform.EventsImpl;
 import net.rebel459.unified.platform.client.ClientEventsImpl;
-import net.rebel459.unified.platform.client.UnifiedClientHelpers;
-import net.rebel459.unified.util.mixin.PlayerStructureMusic;
-import org.jspecify.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientLevel.class)
 public abstract class ClientLevelMixin {
