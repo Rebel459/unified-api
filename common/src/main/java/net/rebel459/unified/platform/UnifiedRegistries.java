ef79bdb0d6646d568cd5cc1d6f058fd282b9f053
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-07-23 15:59:43 +1000

    Update UnifiedRegistries.java

--- common/src/main/java/net/rebel459/unified/platform/UnifiedRegistries.java
+++ common/src/main/java/net/rebel459/unified/platform/UnifiedRegistries.java
@@ -112,11 +112,6 @@ public class UnifiedRegistries {
             public BlockSet.RegistryBuilder blockSet(String name, BlockPreset preset, MapColor color) {
                 return new BlockSet.RegistryBuilder(Identifier.fromNamespaceAndPath(this.modId, name), color, preset, this.blockRegistry);
             }
-
-            @Deprecated
-            public BlockSet.RegistryBuilder blockSet(String name, BlockPreset preset, MapColor color, float hardness, float blastResistance) {
-                return new BlockSet.RegistryBuilder(Identifier.fromNamespaceAndPath(this.modId, name), color, hardness, blastResistance, preset, this.blockRegistry);
-            }
         }
 
         void addAlias(Identifier convertedFrom, Identifier convertedTo);
