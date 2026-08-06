9d3c5080caf1e43d305f16b8a2a47c65e5185b47
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-06 15:05:54 +1000

    Update ColoredBlockPreset.java

--- common/src/main/java/net/rebel459/unified/api/builder/ColoredBlockPreset.java
+++ common/src/main/java/net/rebel459/unified/api/builder/ColoredBlockPreset.java
@@ -1,13 +1,8 @@
 package net.rebel459.unified.api.builder;
 
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CandleBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
-
-import java.util.Optional;
 
 public final class ColoredBlockPreset {
 
@@ -22,7 +17,7 @@ public final class ColoredBlockPreset {
 
     public static final ColoredBlockPreset WOOL = create()
             .properties(() -> BlockBehaviour.Properties.ofFullCopy(Blocks.WOOL.white()))
-            .setFlammability(30, 60)
+            .setFlammability(30, 60, 100)
             .build();
 
     public static final ColoredBlockPreset TERRACOTTA = create()
