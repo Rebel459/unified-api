e1193794145babd867648a7e47047747b3392cf9
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-06 23:40:03 +1000

    don't crash colored sets w/o creative entries

--- common/src/main/java/net/rebel459/unified/impl/builder/ColoredBlockSetProperties.java
+++ common/src/main/java/net/rebel459/unified/impl/builder/ColoredBlockSetProperties.java
@@ -36,6 +36,7 @@ public class ColoredBlockSetProperties {
     private static void creativeEntries(List<ColoredBlockSet> coloredBlockSets) {
         for (ColoredBlockSet coloredBlockSet : coloredBlockSets) {
             ColoredBlockSet.PrecedingCreativeEntries entries = CREATIVE_ENTRIES.get(coloredBlockSet.getId());
+            if (entries == null) continue;
             UnifiedHelpers.CREATIVE_ENTRIES.insertAfter(
                     CreativeModeTabs.COLORED_BLOCKS,
                     entries.colored().get(),
--- common/src/main/java/net/rebel459/unified/impl/builder/ColoredItemSetProperties.java
+++ common/src/main/java/net/rebel459/unified/impl/builder/ColoredItemSetProperties.java
@@ -55,6 +55,7 @@ public class ColoredItemSetProperties {
     private static void creativeEntries(List<ColoredItemSet> coloredItemSets) {
         for (ColoredItemSet coloredItemSet : coloredItemSets) {
             ColoredItemSet.PrecedingCreativeEntries entries = CREATIVE_ENTRIES.get(coloredItemSet.getId());
+            if (entries == null) continue;
             UnifiedHelpers.CREATIVE_ENTRIES.insertAfter(
                     entries.firstTab().getFirst(),
                     entries.firstTab().getSecond().get(),
