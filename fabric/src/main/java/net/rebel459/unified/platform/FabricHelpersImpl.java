bfede504e8f584cd6bdb2d76d10557b261282674
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-07 21:26:13 +1000

    Update FabricHelpersImpl.java

--- fabric/src/main/java/net/rebel459/unified/platform/FabricHelpersImpl.java
+++ fabric/src/main/java/net/rebel459/unified/platform/FabricHelpersImpl.java
@@ -163,7 +163,7 @@ public class FabricHelpersImpl {
         public static PackActivationType getActivationType(PackType info) {
             return switch (info) {
                 case REQUIRED_DATA, REQUIRED_RESOURCES -> PackActivationType.ALWAYS_ENABLED;
-                case OPTIONAL_DATA, OPTIONAL_RESOURCES -> PackActivationType.DEFAULT_ENABLED;
+                case OPTIONAL_DATA, OPTIONAL_RESOURCES -> PackActivationType.NORMAL;
             };
         }
     }
