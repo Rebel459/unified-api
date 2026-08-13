0cd3ff5182d629787b8fd290e56470d23c03c32f
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-08-13 13:16:52 +1000

    Update UnifiedCreativeModeTabs.java

--- common/src/main/java/net/rebel459/unified/api/registry/UnifiedCreativeModeTabs.java
+++ common/src/main/java/net/rebel459/unified/api/registry/UnifiedCreativeModeTabs.java
@@ -1,15 +1,17 @@
 package net.rebel459.unified.api.registry;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.rebel459.unified.impl.platform.CommonPlatform;
 import net.rebel459.unified.impl.platform.PlatformHandler;
-import net.rebel459.unified.platform.InternalHandlerImpl;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public class UnifiedCreativeModeTabs {
@@ -32,11 +34,13 @@ public class UnifiedCreativeModeTabs {
 
     public static class Builder {
         private static final CreativeModeTab.DisplayItemsGenerator EMPTY_GENERATOR = (parameters, output) -> {};
+        private static final Map<String, List<Item>> ITEMS = new ConcurrentHashMap<>();
         private CreativeModeTab.Row row;
         private int column;
         private Component displayName = Component.empty();
         private Supplier<ItemStack> iconGenerator = () -> ItemStack.EMPTY;
         private CreativeModeTab.DisplayItemsGenerator displayItemsGenerator;
+        private String displayItemsModId;
         private boolean canScroll;
         private boolean showTitle;
         private boolean alignedRight;
@@ -62,8 +66,13 @@ public class UnifiedCreativeModeTabs {
             return this;
         }
 
-        public Builder displayItems(final CreativeModeTab.DisplayItemsGenerator displayItemsGenerator) {
-            this.displayItemsGenerator = displayItemsGenerator;
+        public Builder displayItems(final CreativeModeTab.DisplayItemsGenerator generator) {
+            this.displayItemsGenerator = generator;
+            return this;
+        }
+
+        public Builder displayAllFrom(String modId) {
+            this.displayItemsModId = modId;
             return this;
         }
 
@@ -98,12 +107,41 @@ public class UnifiedCreativeModeTabs {
         }
 
         public CreativeModeTab build() {
-            CreativeModeTab tab = PlatformHandler.INSTANCE.internal().createCreativeModeTab(this.row, this.column, this.displayName, this.iconGenerator, this.displayItemsGenerator);
+            CreativeModeTab tab = PlatformHandler.INSTANCE.internal().createCreativeModeTab(this.row, this.column, this.displayName, this.iconGenerator, getDisplayItemsGenerator());
             tab.alignedRight = this.alignedRight;
             tab.showTitle = this.showTitle;
             tab.canScroll = this.canScroll;
             tab.backgroundTexture = this.backgroundTexture;
             return tab;
+        }
+
+        private CreativeModeTab.DisplayItemsGenerator getDisplayItemsGenerator() {
+            CreativeModeTab.DisplayItemsGenerator generator = this.displayItemsGenerator;
+            String modId = this.displayItemsModId;
+
+            return (parameters, output) -> {
+                generator.accept(parameters, output);
+
+                if (modId != null) {
+                    for (Item item : getItems(modId)) {
+                        output.accept(item);
+                    }
+                }
+            };
+        }
+
+        private static List<Item> getItems(String modId) {
+            return ITEMS.computeIfAbsent(modId, id -> {
+                List<Item> items = new ArrayList<>();
+
+                for (Map.Entry<ResourceKey<Item>, Item> entry : BuiltInRegistries.ITEM.entrySet()) {
+                    if (entry.getKey().identifier().getNamespace().equals(id)) {
+                        items.add(entry.getValue());
+                    }
+                }
+
+                return List.copyOf(items);
+            });
         }
     }
 }
